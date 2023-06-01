efa169139e04ceedb9c1ac31562573d4816fb8a0
GreenRiver05 <gabrielbd94@gmail.com>
2023-05-31 23:05:23 -0300

    Formulario
     ultimos estilos, y se comienza con el JavaScript

---
+++ script.js
@@ -0,0 +1,12 @@
+function validarFormulario(){
+let nombre = document.querySelector("#nombre");
+let telefono = document.querySelector("#telefono");
+let correo = document.querySelector("#correo");
+let mensaje = document.querySelector("#mensaje");
+
+
+
+
+
+
+}
