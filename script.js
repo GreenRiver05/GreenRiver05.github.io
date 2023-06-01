d80e8fa22020eb3d078eb825fe98d714a5238c2a
GreenRiver05 <gabrielbd94@gmail.com>
2023-06-01 01:42:39 -0300

    mas estilo

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function validarFormulario(){
+// creamos las variables
 let nombre = document.querySelector("#nombre");
 let telefono = document.querySelector("#telefono");
 let correo = document.querySelector("#correo");
 let mensaje = document.querySelector("#mensaje");
-
+let error = document.querySelector()
 
 
 
